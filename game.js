02bdc0cd855a60ad6fb437897fe31f954da37c04
Samiul <samiulislam@gmail.com>
2020-11-18 18:17:13 +0900

    initial commit

---
+++ game.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    var app = new Vue({
+        el: '#vue-app',
+        data: {
+            message: 'Welcome to punchgame'
+        },
+        methods: {
+
+        },
+        computed: {
+
+        }
+    })
+}
